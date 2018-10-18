3d4ce9aa894842df90081063e970cc70667bd23a
StijnMahieu <stijn.mahieu2@student.howest.be>
2018-10-18 20:31:19 +0200

    Added RotatingTunnel and Teleporters

---
+++ Team04_CaptainToad/Assets/Scripts/TeleporterScript.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterScript : MonoBehaviour {
+
+    [SerializeField]
+    private CharacterController _playerToad;
+
+    private Vector3 _TeleporterBasePosition;
+    private Vector3 _TeleporterTopPosition;
+
+    [SerializeField]
+    private GameObject _baseTeleporter;
+    [SerializeField]
+    private GameObject _topTeleporter;
+
+    [SerializeField]
+    private float _teleportTimer;
+
+    private bool _teleportCoolDown = false;
+
+    private Vector3 _movement;
+    
+    // Use this for initialization
+    void Start ()
+    {
+        //store teleporterpositions in 2 seperate vectors
+        _TeleporterBasePosition = _baseTeleporter.transform.position;
+        _TeleporterTopPosition = _topTeleporter.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //testcode
+        _movement = new Vector3(Input.GetAxis("Horizontal"), -500, Input.GetAxis("Vertical"))*Time.deltaTime;
+        PlayerMovement();
+        //testcode
+
+        if (_teleportCoolDown == true)
+        {
+            _teleportTimer -= Time.deltaTime;
+            Debug.Log(_teleportTimer);
+            if (_teleportTimer <= 0)
+            {
+                _teleportCoolDown = false;
+            }
+        }
+    }
+
+    //testcode
+    private void PlayerMovement()
+    {
+        _playerToad.Move(_movement);
+    }
+    //testcode
+
+    private void OnTriggerStay(Collider _collision)
+    {
+        if (_collision.gameObject.tag == "TeleporterBase" && _teleportCoolDown == false)
+        {
+            TeleportUp();
+            _teleportCoolDown = true;
+            _teleportTimer = 5.0f;
+        }
+        if (_collision.gameObject.tag == "TeleporterTop" && _teleportCoolDown == false)
+        {
+            TeleportDown();
+            _teleportCoolDown = true;
+            _teleportTimer = 5.0f;
+        }
+    }
+    //change between teleporters
+    private void TeleportUp()
+    {
+        _playerToad.transform.position = _TeleporterTopPosition;
+    }
+    private void TeleportDown()
+    {
+        _playerToad.transform.position = _TeleporterBasePosition;
+    }
+}
---
+++ Team04_CaptainToad/Assets/Scripts/TunnelRotationScript.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelRotationScript : MonoBehaviour
+{
+    private float _turningDegrees = 0;
+
+    [SerializeField]
+    private float _rotationSpeed;
+    private bool _isNinety = false;
+    private bool _isOneEighty = false;
+    private bool _istTwoSeventy = false;
+    private bool _isTurning = false;
+
+    [SerializeField]
+    private float _timer = 3.0f;
+
+    void Update()
+    {
+        _timer -= Time.deltaTime;
+        if(_timer <= 0)
+        {
+            _isTurning = true;
+            RotateObject();
+        }
+    }
+
+    private void RotateObject()
+    {
+        if(_isTurning == true)
+        {
+            _turningDegrees += _rotationSpeed;
+
+            if(_turningDegrees >= 90 && _isNinety == false)
+            {
+                _turningDegrees = 90;
+                _isTurning = false;
+                _isNinety = true;
+                _timer = 3.0f;
+            }
+
+            if (_turningDegrees >= 180 && _isOneEighty == false)
+            {
+                _turningDegrees = 180;
+                _isTurning = false;
+                _isOneEighty = true;
+                _timer = 3.0f;
+            }
+
+            if (_turningDegrees >= 270 && _istTwoSeventy == false)
+            {
+                _turningDegrees = 270;
+                _isTurning = false;
+                _istTwoSeventy = true;
+                _timer = 3.0f;
+            }
+
+            if (_turningDegrees >= 360)
+            {
+                _isTurning = false;
+                _turningDegrees = 0;
+
+                _isNinety = false;
+                _isOneEighty = false;
+                _istTwoSeventy = false;
+                _timer = 3.0f;
+            }
+
+            this.transform.eulerAngles = new Vector3(0, _turningDegrees, 0);
+        }
+    }
+}
