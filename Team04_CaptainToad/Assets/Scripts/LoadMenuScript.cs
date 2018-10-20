fa1866e4e24893ef02d73e12930c8335cc51139d
StijnMahieu <stijn.mahieu2@student.howest.be>
2018-10-20 12:12:02 +0200

    Added Skybox and level selection script

---
+++ Team04_CaptainToad/Assets/Scripts/LoadMenuScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadMenuScript : MonoBehaviour {
+
+    private bool _level1Completed = false;
+    private bool _level2Completed = false;
+    private bool _level3Completed = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
