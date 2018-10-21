4ef775b00d9bb3a57c7c7c6e062bbb7cbec1b6ea
StickieBE <pieterdestickere@gmail.com>
2018-10-21 21:39:47 +0200

    - CharacterController first version.

---
+++ Team04_CaptainToad/Assets/Scripts/Player/CharControl.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharControl : MonoBehaviour {
+
+    #region Variables
+
+    // Public variables
+    public static CharControl Instance;
+    public GameObject CameraY;
+
+    // readonly variables
+    private readonly float speedRotation = 10;
+    private readonly float mass = 3.0F; // defines the character mass
+
+    // Private variables
+    private float movementSpeed = 7.5f;
+    private float gravity = 10f;
+    private Vector3 impact = Vector3.zero;
+    private float activeSpeed;
+    private float fallingMultiplier;
+    private CharacterController controller;
+
+    #endregion
+
+    #region Properties
+
+    public bool IsRunning { get; private set; }
+    public Vector3 Move { get; private set; }
+
+    #endregion
+
+    #region Methods
+
+    void Start()
+    {
+        Instance = this;
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            Move = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+
+            if (Input.GetButton("Run"))
+            {
+                movementSpeed *= 1.5f;
+            }
+
+            Move *= movementSpeed;
+        }
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the Move is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        Move = new Vector3(Move.x, (Move.y - gravity * Time.deltaTime), Move.z);
+
+        // Move the controller
+        controller.Move(Move * Time.deltaTime);
+
+        /*
+        if (Move != Vector3.zero)
+            controller.transform.forward = new Vector3(Move.x, 0f, Move.z);
+
+        if (Move != Vector3.zero)
+            controller.transform.rotation = Quaternion.LookRotation(Move);
+        */
+    }
+
+    public void AddImpact(Vector3 dir, float force) {
+        dir.Normalize();
+        if (dir.y < 0) dir.y = -dir.y; // reflect down force on the ground
+        impact += dir.normalized * force / mass;
+        fallingMultiplier = .5f;
+    }
+
+    public void TargetRotation(Quaternion target) {
+        // Keeps character aligned with forward camera axis
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * speedRotation); //lerp rotation to given rotation
+    }
+
+    private Camera GetPlayerCam()
+    {
+        Camera _c = null;
+        foreach (Camera c in Camera.allCameras)
+        {
+            if (c.name == "PlayerCam") _c = c;
+        }
+        if (_c == null) _c = Camera.main;
+        //Debug.Log("Active camera: " + _c);
+        return _c;
+    }
+
+    #endregion
+}
---
+++ Team04_CaptainToad/Assets/Scripts/Various/ThirdPersonControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonControl : MonoBehaviour {
+
+    public CharControl CharControl;
+    public Vector2 LimitY = new Vector2(5, 85);
+    public float SpeedRotate = 180;
+    public float Speed = 5f;
+    public Transform CameraY;
+    private CharacterController _controller;
+
+    public Transform Target;
+
+    private float _yRotation = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        _controller = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessCameraMovements();
+    }
+
+    void ProcessCameraMovements()
+    {
+        
+        // Handles right joystick input
+        CameraY.transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal_R"), 0) * SpeedRotate * Time.deltaTime);
+
+        /*
+        _yRotation += Input.GetAxis("Vertical_R") * SpeedRotate * Time.deltaTime;
+        _yRotation = Mathf.Clamp(_yRotation, LimitY.x, LimitY.y);
+        CameraY.eulerAngles = new Vector3(_yRotation, CameraY.eulerAngles.y, CameraY.eulerAngles.z);
+        */
+
+
+        CameraY.transform.LookAt(Target);
+        //CameraY.transform.Translate(Vector3.right * Time.deltaTime);
+        
+    }
+}
